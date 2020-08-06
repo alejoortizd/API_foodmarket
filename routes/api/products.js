751f8f8f2531b7129cc1fb5eba97215f7295a4ea
Alejo Ortiz <davidalejandro64@gmail.com>
2020-08-06 18:01:07 -0500

    comment new function

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,9 +2,9 @@ const {Router} = require('express');
 const router = Router();
 const productService = require('../../lib/mongoProducts')
 
-router.get('/', productService.getProducts)
+router.get('/', productService.getProducts);
 //router.post('/', productService.createProduct)
-router.get('/:productId', productService.getProduct)
+router.get('/:productId', productService.getProduct);
 //router.put('/:productId', productService.updateProduct)
 //router.delete('/:productId', productService.deleteProduct)
 
