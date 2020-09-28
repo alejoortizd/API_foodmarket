e7b405d8a9b7044304a67501dc34e1edae6fac19
unknown <davidalejandro64@gmail.com>
2020-09-28 11:07:59 -0500

    modified app.liste

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const app = require('./server')
 const DataBase = require('./config/db');
+const { config } = require('./config/index');
 
 // Connect to Database
 DataBase.connect();
 
 // server
-const server = app.listen(app.get('port'), () => {
-    console.log(`Listen http://localhost:${server.address().port}`);
+
+const host = '0.0.0.0';
+const port = config.port;
+app.listen(port, host, () => {
+    console.log(`El servidor esta funcionando en el puerto: ${port}`);
 })
